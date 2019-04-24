0d7920fb61da734945f98526cb5a4e1755349c3f
hansenjl <jenn.leigh.hansen@gmail.com>
2019-04-24 10:04:16 -0700

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :vets
   root 'sessions#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
@@ -10,5 +11,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :pets
+
+  resources :vets do
+    resources :pets, only: [:new, :create, :index]
+  end
+
+
 
 end
