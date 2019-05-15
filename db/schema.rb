6c9651ef393193b0a155b86c1f46fceb050d487a
hansenjl <jenn.leigh.hansen@gmail.com>
2019-05-15 08:43:34 -0700

    Add Reviews Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_175405) do
+ActiveRecord::Schema.define(version: 2019_05_15_150949) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_05_02_175405) do
     t.integer "age"
     t.integer "vet_id"
     t.integer "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.integer "vet_id"
+    t.integer "reviewer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
